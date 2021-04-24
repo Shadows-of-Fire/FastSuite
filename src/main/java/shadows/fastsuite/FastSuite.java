328a6f2eff7fa4be73f23c097897f0006e2d1747
Brennan Ward <Bward7864@gmail.com>
2021-04-24 15:43:14 -0400

    test case (commented out)

--- src/main/java/shadows/fastsuite/FastSuite.java
+++ src/main/java/shadows/fastsuite/FastSuite.java
@@ -16,6 +16,7 @@ public class FastSuite {
 	public static final Logger LOG = LogManager.getLogger(MODID);
 
 	public FastSuite() {
+		//	MinecraftForge.EVENT_BUS.addListener(this::test);
 	}
 
 	/**
@@ -30,4 +31,44 @@ public class FastSuite {
 		}));
 	}
 
+	/*
+		public void test(FMLServerStartedEvent e) {
+			RecipeManager mgr = e.getServer().getRecipeManager();
+			CraftingInventory inv = new CraftingInventory(new Container(null, -1) {
+				public boolean canInteractWith(PlayerEntity playerIn) {
+					return false;
+				}
+			}, 2, 2);
+			World world = e.getServer().getWorld(World.OVERWORLD);
+			inv.setInventorySlotContents(0, new ItemStack(Items.ACACIA_LOG));
+			CraftingInventory inv2 = new CraftingInventory(new Container(null, -1) {
+				public boolean canInteractWith(PlayerEntity playerIn) {
+					return false;
+				}
+			}, 2, 2);
+			inv2.setInventorySlotContents(0, new ItemStack(Items.BIRCH_PLANKS));
+			inv2.setInventorySlotContents(2, new ItemStack(Items.BIRCH_PLANKS));
+	
+			CraftingInventory inv3 = new CraftingInventory(new Container(null, -1) {
+				public boolean canInteractWith(PlayerEntity playerIn) {
+					return false;
+				}
+			}, 2, 2);
+			for (int i = 0; i < 4; i++)
+				inv3.setInventorySlotContents(i, new ItemStack(Items.OAK_PLANKS));
+	
+			CraftingInventory[] arr = { inv, inv2, inv3 };
+			String[] names = { "acacia planks", "sticks", "crafting table" };
+	
+			long time, time2;
+	
+			for (int i = 0; i < 192; i++) {
+				int j = i % 3;
+				time = System.nanoTime();
+				mgr.getRecipe(IRecipeType.CRAFTING, arr[j], world);
+				time2 = System.nanoTime();
+				LOG.info("Took {} ns to find the recipe for {} on attempt {}", time2 - time, names[j], i);
+			}
+		}
+	*/
 }
