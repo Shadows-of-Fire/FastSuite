1d41ba257b9f2fc6c8f450ad6925388b512dafef
Brennan Ward <Bward7864@gmail.com>
2022-03-27 04:04:16 -0400

    1.18.2

--- src/main/java/shadows/fastsuite/mixin/ServerResourcesMixin.java
+++ src/main/java/shadows/fastsuite/mixin/ServerResourcesMixin.java
@@ -8,18 +8,18 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.server.ServerResources;
+import net.minecraft.server.ReloadableServerResources;
 import net.minecraft.world.item.crafting.RecipeManager;
 import shadows.fastsuite.AuxRecipeManager;
 
-@Mixin(ServerResources.class)
+@Mixin(ReloadableServerResources.class)
 public class ServerResourcesMixin {
 
 	@Shadow
 	private final RecipeManager recipes = new AuxRecipeManager();
 
-	@Inject(at = @At("TAIL"), method = "loadResources(Ljava/util/List;Lnet/minecraft/core/RegistryAccess;Lnet/minecraft/commands/Commands$CommandSelection;ILjava/util/concurrent/Executor;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;", cancellable = true)
-	private static void func_240961_a_(CallbackInfoReturnable<CompletableFuture<ServerResources>> info) {
+	@Inject(at = @At("TAIL"), method = "loadResources(Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/core/RegistryAccess$Frozen;Lnet/minecraft/commands/Commands$CommandSelection;ILjava/util/concurrent/Executor;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;", cancellable = true)
+	private static void loadResources(CallbackInfoReturnable<CompletableFuture<ReloadableServerResources>> info) {
 		info.setReturnValue(info.getReturnValue().thenApply(dpr -> {
 			((AuxRecipeManager) dpr.getRecipeManager()).processInitialRecipes(dpr.getRecipeManager().recipes);
 			return dpr;
