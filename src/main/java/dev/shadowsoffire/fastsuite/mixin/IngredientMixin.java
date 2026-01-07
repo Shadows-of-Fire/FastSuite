03303db86f3894b7e0fe5bb5b95b2baf61e9c994
Shadows_of_Fire <3682588+Shadows-of-Fire@users.noreply.github.com>
2026-01-06 23:02:58 -0500

    WAR mixin bug and modernfix compat

--- src/main/java/dev/shadowsoffire/fastsuite/mixin/IngredientMixin.java
+++ src/main/java/dev/shadowsoffire/fastsuite/mixin/IngredientMixin.java
@@ -4,6 +4,7 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntComparators;
@@ -12,7 +13,7 @@ import net.minecraft.world.entity.player.StackedContents;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 
-@Mixin(Ingredient.class)
+@Mixin(value = Ingredient.class, priority = 500)
 public abstract class IngredientMixin {
 
     @Shadow
@@ -24,7 +25,23 @@ public abstract class IngredientMixin {
      * Without this, it is possible for multiple threads to enter the method simultaneously and return null from this method.
      */
     @Overwrite
+    @SuppressWarnings("unused")
     public IntList getStackingIds() {
+        Object ignored = this.getItems(); // Need to call this to make ModernFix's mixin happy otherwise the world explodes
+        return this.__getStackingIds();
+    }
+
+    @Shadow(remap = false)
+    public abstract boolean checkInvalidation();
+
+    @Shadow(remap = false)
+    protected abstract void markValid();
+
+    @Shadow
+    public abstract ItemStack[] getItems();
+
+    @Unique
+    private IntList __getStackingIds() {
         synchronized (this) {
             if (this.stackingIds == null || checkInvalidation()) {
                 this.markValid();
@@ -41,13 +58,4 @@ public abstract class IngredientMixin {
             return this.stackingIds;
         }
     }
-
-    @Shadow
-    public abstract boolean checkInvalidation();
-
-    @Shadow
-    protected abstract void markValid();
-
-    @Shadow
-    public abstract ItemStack[] getItems();
 }
